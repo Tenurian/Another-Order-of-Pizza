89ea74d7e63ab2b8e89a6bc6722f5881825a5ce1
brandon berning <brandon berning>
2016-01-29 10:44:52 -0700

    added json

--- script.js
+++ script.js
@@ -3,16 +3,28 @@
 
 var console = window.console;
 
+var request = new XMLHttpRequest();
+request.open("GET", "jsonfile.json", false);
+request.send(null)
+var my_JSON_object = JSON.parse(request.responseText);
+console.log(my_JSON_object);
+
 $('.topping').on('click', function () {
     console.log("something happened");
     if (this.checked) {
-//        console.log($(this).val());
+        //        console.log($(this).val());
         //here is where we'd put the toppings on the image and load them into the list
         var lst = "";
-        if(document.getElementById("list").innerHTML.toString() === ""){
+        if (document.getElementById("list").innerHTML.toString() === "") {
             console.log("list is empty");
         } else {
-            console.log( document.getElementById("list").innerHTML.toString() );
+            $.getJSON("yourjsonfile.json", function (data) {
+                console.log("JSON Data: " + data);
+                $.each(data, function (key, val) {
+                    console.log(key + "value:: " + val);
+                });
+            });
+            console.log(document.getElementById("list").innerHTML.toString());
             lst = document.getElementById("list").innerHTML.toString();
         }
         lst += "<p id='list_" + $(this).val() + "'>" + $(this).val() + "</p>";
@@ -20,6 +32,6 @@ $('.topping').on('click', function () {
         console.log(lst);
     } else {
         console.log("unclicked");
-        document.getElementById("list").removeChild(document.getElementById("list_"+$(this).val()));
+        document.getElementById("list").removeChild(document.getElementById("list_" + $(this).val()));
     }
 });
